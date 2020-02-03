702ff58c5e362a63a2001c1d12eb3dbe2357d3d5
jake <jakewillison23@gmail.com>
2020-02-03 10:53:35 -0500

    Added Initial Movement
    Created initial functionality for the car object to move between different points.

---
+++ Assets/move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    //int x = transform.position.x;
+    //int y = transform.position.y;
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
+    public GameObject car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        car.transform.position = Vector3.MoveTowards(car.transform.position, target1.transform.position, 5*Time.deltaTime);
+
+    }
+}
